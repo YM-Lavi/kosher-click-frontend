c89bfbd23727705858f72854c343f7151a18f8c0
Yosi Lavi <yoseflavi98@gmail.com>
2025-12-30 17:51:10 +0200

    Update Vite config to define process.env and React
    Added process.env and React definition to Vite config.

--- src/vite.config.js
+++ src/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'process.env': {},
+    // השורה הבאה פותרת את השגיאה שראינו ב-Console
+    'React': 'react' 
+  }
 })
