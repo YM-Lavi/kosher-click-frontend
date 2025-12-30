0fe36a9d0b53344743db5729b0726ee9b9f7befa
Yosi Lavi <yoseflavi98@gmail.com>
2025-12-30 18:42:17 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    // השורה הזו פותרת את השגיאה "React is not defined"
     jsxInject: `import React from 'react'`
   }
 })
