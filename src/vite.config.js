4b4a7731fba949204e90a10dcb884d683a8a7877
Yosi Lavi <yoseflavi98@gmail.com>
2025-12-30 18:55:53 +0200

    Remove Vite configuration for React

--- src/vite.config.js
+++ src/vite.config.js
@@ -1,11 +1,1 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-  plugins: [react()],
-  define: {
-    'process.env': {},
-    // השורה הבאה פותרת את השגיאה שראינו ב-Console
-    'React': 'react' 
-  }
-})
